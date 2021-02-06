72cc152e3ec5b803f00ff86049fb4003b4982398
biagavirete <biagavirete@gmail.com>
2021-02-06 17:29:52 -0300

    feat: call post method with form inputs

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import api from '../../services/api';
+import { getPosts } from '../../store/ducks/posts/actions';
 
 const Form = () => {
+  const dispatch = useDispatch();
+
+  let inputImageUrl = useRef<HTMLInputElement>(null);
+  let inputImageDescription = useRef<HTMLInputElement>(null);
+
+  const refreshPosts = () => {
+    api.get('/posts')
+      .then(response => dispatch(getPosts(response.data)))
+  }
+
+  const { name, userPicture } = useSelector((state: any) => state.user);
+
+  const newPost = () => {
+    const imageUrl = inputImageUrl.current?.value;
+    const imageDescription = inputImageDescription.current?.value;
+
+    api.post('/posts', {
+      user: name,
+      userPicture: userPicture,
+      postPicture: imageUrl,
+      description: imageDescription,
+      likes: 0
+    }).then(response => {
+      if (response.status === 201) {
+        refreshPosts();
+      }
+    })
+  }
+
   return (
     <div className="form">
-      <p>Faça uma postagem aqui</p>
-      <input type="text" placeholder="Cole a url da imagem" />
-      <input type="text" placeholder="Digite uma descrição" />
-      <button>Postar!</button>
+      <p>New post</p>
+      <input type="text" placeholder="Image URL" ref={inputImageUrl} />
+      <input type="text" placeholder="Description" ref={inputImageDescription} />
+      <button onClick={newPost}>Post!</button>
     </div>
   )
 }
