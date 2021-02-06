eee3ed36fdd83f0bfa0dca31e108c9cefeab0925
biagavirete <biagavirete@gmail.com>
2021-02-06 14:11:02 -0300

    feat: add provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { Provider } from "react-redux";
 import './App.css';
 import Header from "./components/Header";
 import Main from "./components/Main";
+import { store } from "./store";
 
 function App() {
   return (
     <>
-      <Header />
-      <Main />
+      <Provider store={store}>
+        <Header />
+        <Main />
+      </Provider>
     </>
   );
 }
