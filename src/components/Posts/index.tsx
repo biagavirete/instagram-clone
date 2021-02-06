8ba03e4bc4004ce7a7b54c0b9f6fee56a7a391d5
biagavirete <biagavirete@gmail.com>
2021-02-06 14:11:11 -0300

    feat: get posts from store

--- src/components/Posts/index.tsx
+++ src/components/Posts/index.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FiHeart } from 'react-icons/fi';
+import { useDispatch, useSelector } from 'react-redux';
+import api from '../../services/api';
+import { getPosts } from '../../store/ducks/posts/actions';
+import { IPostItem, IPostsState } from '../../store/ducks/posts/types';
 
 export const Posts = () => {
+
+  const dispatch = useDispatch();
+
+  const posts = useSelector((state: IPostsState) => state.posts.postsList)
+
+  useEffect(() => {
+    api.get('/posts')
+      .then(response => dispatch(getPosts(response.data)))
+  }, [])
+
+  console.log(posts)
+
   return (
-    <div className="post">
-      <header>
-        <img src="https://randomuser.me/api/portraits/women/17.jpg" alt="user" />
-        <div className="post-user">
-          <strong>nasa</strong>
-          <span>Mars</span>
+    <>
+      { posts !== undefined && posts.map((post: IPostItem) => (
+        <div className="post">
+          <header>
+            <img src={post.userPicture} alt="user" />
+            <div className="post-user">
+              <strong>{post.user}</strong>
+              <span>Mars</span>
+            </div>
+          </header>
+          <div className="post-image">
+            <img src={post.postPicture} alt="post" />
+          </div>
+          <div className="post-likes">
+            <FiHeart /> {post.likes}
+          </div>
+          <p>{post.description}</p>
         </div>
-      </header>
-      <div className="post-image">
-        <img src="https://www.publicdomainpictures.net/pictures/90000/velka/mars.jpg" alt="post" />
-      </div>
-      <div className="post-likes">
-        <FiHeart />
-      </div>
-      <p>Waving goodbye to a spacecraft.</p>
-    </div>
+      ))}
+    </>
   )
 }
 
