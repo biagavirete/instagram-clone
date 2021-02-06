233711c6d455914b63e654520315764905c04182
biagavirete <biagavirete@gmail.com>
2021-02-06 12:03:07 -0300

    feat: get stories data from api

--- src/components/Stories/index.tsx
+++ src/components/Stories/index.tsx
@@ -1,28 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from '../../services/api';
+
+interface IStory {
+  id: number;
+  user: string;
+  userPicture: string;
+  time: string;
+}
 
 const Stories = () => {
+  const [stories, setStories] = useState<IStory[]>([]);
+
+  useEffect(() => {
+    api.get('/stories')
+      .then(response => setStories(response.data))
+  }, [])
   return (
     <div className="stories">
       <h2>Stories</h2>
-
-      <div className="storie">
-        <div className="storie-image">
-          <img src="https://randomuser.me/api/portraits/men/11.jpg" alt="user" />
-        </div>
-        <div className="storie-user">
-          <strong>history</strong>
-          <span>há 2 horas</span>
-        </div>
-      </div>
-      <div className="storie">
-        <div className="storie-image">
-          <img src="https://randomuser.me/api/portraits/men/11.jpg" alt="user" />
-        </div>
-        <div className="storie-user">
-          <strong>history</strong>
-          <span>há 2 horas</span>
-        </div>
-      </div>
+      {stories !== undefined && stories.map((story: IStory) => (
+        <>
+          <div className="storie" key={story.id}>
+            <div className="storie-image">
+              <img src={story.userPicture} alt={story.user} />
+            </div>
+            <div className="storie-user">
+              <strong>{story.user}</strong>
+              <span>{story.time}</span>
+            </div>
+          </div>
+        </>
+      ))}
     </div>
   )
 }
